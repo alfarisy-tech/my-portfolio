8f5a8933a2c459c258ff8114c4b767ff40871ba8
alfarisy-tech <ariafsundoro@gmail.com>
2023-11-13 20:09:46 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <section id='home'>
+        <container>
+          <div className='flex flex-wrap pt-36'>
+            <div className=''></div>
+          </div>
+        </container>
+      </section>
+    </>
+  )
+}
